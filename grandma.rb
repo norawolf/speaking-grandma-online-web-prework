3fb5bfe8504a811f5d9f4d22c0bca29ca1e0478a
Nora Vogt <noraevogt@gmail.com>
2018-11-12 22:29:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else 
+  else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
